5f2223580da7792358d7684f1c5c1faacbf00e8a
福間 良 <rfukuma@coresoft-net.co.jp>
2024-06-25 14:17:17 +0900

    fix: 警告を削除

--- composables/api/thresholdAlert/TargetFetch.ts
+++ composables/api/thresholdAlert/TargetFetch.ts
@@ -14,6 +14,7 @@ interface ThresholdAlertTargetFetchResponse
 export class ThresholdAlertTargetFetchRequestFactory
   implements ThresholdAlertTargetFetchRequest
 {
+  // eslint-disable-next-line no-useless-constructor
   constructor(
     public adminAlertId: number = 0,
     public isStore: boolean = false,
