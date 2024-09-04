4db9ba18ea76b5d8a5d7f1fafb918cc812cadd39
福間 良 <rfukuma@coresoft-net.co.jp>
2024-09-04 18:00:36 +0900

    サイドメニューをスライドインする形に修正

--- components/app/Header.vue
+++ components/app/Header.vue
@@ -2,10 +2,21 @@
   <header
     class="flex items-center bg-gradient-to-r from-pink-500 to-violet-400 py-2 pl-1 pr-4"
   >
-    <a href="/" class="text-bold mb-[-2px] inline flex-1 text-slate-200">
-      <img src="/img/logo.png" alt="sensy" class="inline-block h-8" />
-      SENSY MD
-    </a>
+    <div class="flex-1">
+      <div class="inline-block w-44">
+        <a href="/" class="text-bold mb-[-2px] inline text-slate-200">
+          <img src="/img/logo.png" alt="sensy" class="inline-block h-8" />
+          SENSY MD
+        </a>
+      </div>
+      <UButton
+        v-if="status !== 'unauthenticated'"
+        color="gray"
+        @click="onMenuClick"
+      >
+        メニュー
+      </UButton>
+    </div>
     <UButton v-if="status !== 'unauthenticated'" color="gray" @click="logout">
       ログアウト
     </UButton>
@@ -14,6 +25,7 @@
 
 <script setup lang="ts">
 const { status } = useAuth()
+const emit = defineEmits(['onMenuClick'])
 
 async function logout() {
   const { signOut } = useAuth()
@@ -22,5 +34,9 @@ async function logout() {
   })
 
   useRouter().push('/login')
+}
+
+function onMenuClick() {
+  emit('onMenuClick')
 }
 </script>
